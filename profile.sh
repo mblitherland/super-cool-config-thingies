bd437c26b6a30173db0868850710e881a307afd7
Michael Litherland <mlitherland@overdrive.com>
2023-02-03 15:28:47 -0500

    Some profie updates

--- profile.sh
+++ profile.sh
@@ -1,20 +1,25 @@
 
 # set PATH so it includes user's private bin directories
-PATH="$HOME/bin:$HOME/.local/bin:$PATH:/usr/lib/chromium-browser/"
+# PATH="$HOME/bin:$HOME/.local/bin:$PATH:/usr/lib/chromium-browser/"
 
 export WORKON_HOME=~/Envs
-source /usr/local/bin/virtualenvwrapper.sh
+# source /usr/local/bin/virtualenvwrapper.sh
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ git:(\1)/'
 }
 export PS1="\u@\h \[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\n$ "
 #export PS1="\[\033[32m\]\w\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\n$ "
-# This is for running the engage back-end outside of docker
-export SQLALCHEMY_DATABASE_URI=redacted
+
 # Only for OSX
 #if [ -f $(brew --prefix)/etc/bash_completion ]; then
 #  . $(brew --prefix)/etc/bash_completion
 #fi
+
+# Docker things
+# Create this volume first:
+# docker volume create ruby-bundle-cache
+alias docked='docker run --rm -it -v ${PWD}:/rails -v ruby-bundle-cache:/bundle -p 3000:3000 ghcr.io/rails/cli'
+
 dcleanup(){
     docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null
     docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null
@@ -25,14 +30,11 @@ dcleanup_for_real(){
     # Delete all images
     docker rmi $(docker images -q)
 }
-# AWS information, primarily for Lambda
+
+# AWS information
 export AWS_ACCESS_KEY_ID=redacted
 export AWS_SECRET_ACCESS_KEY=redacted
-# for when I want to run the node stuff locally
-export ENGAGE_INTEGRATION_URL=localhost:50051
-export SETTINGS_SERVICE_URL=localhost:50052
-# I don't think this is necessary any longer
+
 export NODE_ENV=development
-export ALTERCARE_OF_MENTOR_BAZMAN_ORG_ID=1
 
 export EDITOR=vim
