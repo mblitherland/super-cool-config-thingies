17a0495be79dcc058d9daf936eabb235b5bfeab8
M Litherland <mlitherland@overdrive.com>
2024-01-30 10:29:16 -0500

    Config file for wezterm

---
+++ wezterm.lua
@@ -0,0 +1,36 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.audible_bell = 'Disabled'
+
+config.default_domain = 'WSL:Ubuntu'
+
+config.initial_cols = 132
+config.initial_rows = 36
+
+config.colors = {
+  visual_bell = '#202020',
+}
+config.visual_bell = {
+  fade_in_function = 'EaseIn',
+  fade_in_duration_ms = 150,
+  fade_out_function = 'EaseOut',
+  fade_out_duration_ms = 150,
+}
+
+-- For example, changing the color scheme:
+-- config.color_scheme = 'AdventureTime'
+
+-- and finally, return the configuration to wezterm
+return config
